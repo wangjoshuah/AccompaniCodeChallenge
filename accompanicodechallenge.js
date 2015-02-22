1ac8200bb8cef6a12b403b9760cab47bfe6f612c
Joshua Wang <wangjoshuah@gmail.com>
2015-02-22 16:15:25 -0500

    Basic Functionality Implemented
    Can read from google calendar and display events on the page

--- accompanicodechallenge.js
+++ accompanicodechallenge.js
@@ -4,5 +4,11 @@ $(document).ready(function() {
 
 	$('#calendar').fullCalendar({
 		// put your options and callbacks here
-	})
+		googleCalendarApiKey: 'AIzaSyDyeKHQLsI4QSoYb27MrFn5MIE9vF7daXk',
+		events: {
+			googleCalendarId: '1ps37k1k1ru9arih6bdkjra2l4@group.calendar.google.com',
+			className: 'gcal-event' // an option!
+		}
+		// weekends: false
+	});
 });
